c7a8335a391297bd8df7bc779593c35bff4d3b6a
mendozg <gabrielemf@hotmail.com>
2019-05-10 19:44:36 -0600

    GlobalBoostCore Client

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -13,7 +13,7 @@
  * for both globalboostd and globalboost-core, to make it harder for attackers to
  * target servers or GUI users specifically.
  */
-const std::string CLIENT_NAME("Satoshi");
+const std::string CLIENT_NAME("GlobalBoostCore");
 
 /**
  * Client version number
