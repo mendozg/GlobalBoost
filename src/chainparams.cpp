8c77f1fcdb5e2ff3af594bccdd3241d9980d91ee
mendozg <gabrielemf@hotmail.com>
2019-05-14 22:38:21 -0600

    Update main Private Key Prefix

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -125,7 +125,7 @@ public:
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,77);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,139);
-        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,128);
+        base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,205);
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
